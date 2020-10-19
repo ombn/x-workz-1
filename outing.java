8c2a1bdb6c7de37849a9315c76da2a56fcacb668
Rohanpatil932 <72926252+Rohanpatil932@users.noreply.github.com>
2020-10-19 20:36:36 +0530

    outing

---
+++ outing.java
@@ -0,0 +1,14 @@
+class Outing{
+public static void main (String[]  ntg){
+System.out.println("chilling");
+
+music();
+drinking();
+}
+public static void music(){
+System.out.println("jazz");
+}
+public static void drinking(){
+System.out.println("NO DRINKS PLEASE");
+}
+}
