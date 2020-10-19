7fbe5c6da413e9b342c98f051a5f7f87e7645bee
Rohanpatil932 <72926252+Rohanpatil932@users.noreply.github.com>
2020-10-19 20:58:20 +0530

    dishes

---
+++ dishes.java
@@ -0,0 +1,26 @@
+class Dishes{
+public static void main (String[] all){
+System.out.println("dishes i love the most");
+delhi();
+mumbai();
+italy();
+germany();
+america();
+}
+public static void delhi(){
+System.out.println("momos");
+}
+public static void mumbai(){
+System.out.println("Vada pav");
+}
+public static void italy(){
+System.out.println("pizza");
+}
+public static void germany (){
+System.out.println("Burger");
+}
+public static void america(){
+System.out.println("Pan cake");
+}
+}
+
